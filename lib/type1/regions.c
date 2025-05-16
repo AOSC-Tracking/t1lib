be1ad2d9a012aee7674dd36999ecc61a2f49647a
Mingcong Bai <jeffbai@aosc.io>
2025-05-16 16:46:09 +0800

    AOSCOS: fix(regions.c): add missing variable declarations for x1, y1
    Fix the following build errors, where GCC >= 14 is more strict about
    implicit variable declarations:
    
    regions.c: In function 't1_ChangeDirection':
    regions.h:48:49: error: type of 'x2' defaults to 'int' [-Wimplicit-int]
       48 | #define   ChangeDirection(type,R,x,y,dy,x2,y2)  t1_ChangeDirection(type,R,x,y,dy,x2,y2)
          |                                                 ^~~~~~~~~~~~~~~~~~
    regions.c:602:6: note: in expansion of macro 'ChangeDirection'
      602 | void ChangeDirection(type, R, x, y, dy, x2, y2)
          |      ^~~~~~~~~~~~~~~
    regions.h:48:49: error: type of 'y2' defaults to 'int' [-Wimplicit-int]
       48 | #define   ChangeDirection(type,R,x,y,dy,x2,y2)  t1_ChangeDirection(type,R,x,y,dy,x2,y2)
          |                                                 ^~~~~~~~~~~~~~~~~~
    regions.c:602:6: note: in expansion of macro 'ChangeDirection'
      602 | void ChangeDirection(type, R, x, y, dy, x2, y2)
          |      ^~~~~~~~~~~~~~~
    
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>

--- lib/type1/regions.c
+++ lib/type1/regions.c
@@ -604,6 +604,7 @@ void ChangeDirection(type, R, x, y, dy, x2, y2)
        register struct region *R;  /* region in which we are changing direction */
        fractpel x,y;         /* current beginning x,y                        */
        fractpel dy;          /* direction and magnitude of change in y       */
+       int x2, y2;
 {
        register fractpel ymin,ymax;  /* minimum and maximum Y since last call */
        register fractpel x_at_ymin,x_at_ymax;  /* their respective X's       */
